618e1f7f31565dc38888301f9867a32c61c31a0d
Bartomiej Król <krolbartlomiej1122@gmail.com>
2025-01-19 12:45:31 +0100

    poprawki lab11

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,27 +1,27 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common'; // Import CommonModule
-import { RouterModule } from '@angular/router'; // Import RouterModule
+import { CommonModule } from '@angular/common'; 
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule], // Dodaj CommonModule i RouterModule
+  imports: [CommonModule, RouterModule], 
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
   constructor(public authService: AuthService, private router: Router) {}
 
   signOut() {
     const logoutObservable = this.authService.logout();
-  
+
     if (logoutObservable) {
       logoutObservable.subscribe({
         next: () => {
           console.log('Wylogowanie zakończone sukcesem');
-          this.router.navigate(['/login']); // Przekierowanie na stronę logowania
+          this.router.navigate(['/login']); 
         },
         error: (err) => {
           console.error('Błąd podczas wylogowywania:', err);
@@ -31,9 +31,8 @@ export class NavbarComponent {
       console.error('Nie można wylogować użytkownika.');
     }
   }
-  
+
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn();
   }
-  
 }
